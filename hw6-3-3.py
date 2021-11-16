4baa005a499bcfd05bfc62b55e238a0257988d16
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-11-16 09:39:08 -0500

    add file

--- hw6-3-3.py
+++ hw6-3-3.py
@@ -1,6 +1,14 @@
 # Author MB 11/12/2021
 
-num = input('give me 5 numbers seperated by spaces: ')
+num = input('give me 5 numbers seperated by spaces:')
 num = num.split()
 
-print(num)
+a = int(num[0])
+b = int(num[1])
+c = int(num[2])
+d = int(num[3])
+e = int(num[4])
+
+total = a + b + c + d + e
+
+print(total)
