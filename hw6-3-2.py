6dae371a0b9be2bf0efc9680128c8aabd02e289b
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-11-16 09:52:03 -0500

    add file

--- hw6-3-2.py
+++ hw6-3-2.py
@@ -1,8 +1,12 @@
 # Author MB 11/12/2021
 
-num = input('give me a bunch of numbers: ')
-num = list(num)
+lst = input('give me a bunch of numbers: ')
+lst = lst.split()
+lst = list(lst)
+lst2 = lst.copy()
+lst.sort()
 
-num.sort()
-
-print(num)
+if lst == lst2:
+    print('the list is sorted')
+else:
+    print('the list is not sorted')
